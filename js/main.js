d0ac711e2143f7343e2abc7f07d5ed6259bf60cc
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-03-17 19:29:44 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,88 +1,35 @@
-"use strict"
-
-// canvasの設定（せってい）
-let canvas = document.getElementById( 'canvas' );
-canvas.width = 640;		//canvasの横幅（よこはば）
-canvas.height = 640;	//canvasの縦幅（たてはば）
- 
-//コンテキストを取得（しゅとく）
-let ctx = canvas.getContext( '2d' );
- 
-//りこちゃんのオブジェクトを作成
-let rico = new Object();
-rico.img = new Image();
-rico.img.src = 'img/pic.png';
-rico.x = 0;
-rico.y = 0;
-rico.move = 0;
-
-//キーボードのオブジェクトを作成
-let key = new Object();
-key.up = false;
-key.down = false;
-key.right = false;
-key.left = false;
-key.push = '';
- 
-//メインループ
-function main() {
-	//塗（ぬ）りつぶす色を指定（してい）
-	ctx.fillStyle = "rgb( 0, 0, 0 )";
-	//塗（ぬ）りつぶす
-	ctx.fillRect(0, 0, 640, 640);
-	//画像を表示
-	ctx.drawImage( rico.img, rico.x, rico.y );
-	addEventListener("keydown", keydownfunc, false);
-	addEventListener("keyup", keyupfunc, false);
-
-	//方向キーが押されている場合（ばあい）は、りこちゃんが移動する
-	if ( rico.move === 0 ) {
-		if ( key.left === true ) {
-			rico.move = 32;
-			key.push = 'left';
-		}
-		if ( key.up === true ) {
-			rico.move = 32;
-			key.push = 'up';
-		}
-		if ( key.right === true ) {
-			rico.move = 32;
-			key.push = 'right';
-		}
-		if ( key.down === true ) {
-			rico.move = 32;
-			key.push = 'down';
-		}
-	}
-	//rico.moveが0より大きい場合は、4pxずつ移動（いどう）を続ける
-	if (rico.move > 0) {
-		rico.move -= 4;
-		if ( key.push === 'left' ) rico.x -= 4;
-		if ( key.push === 'up' ) rico.y -= 4;
-		if ( key.push === 'right' ) rico.x += 4;
-		if ( key.push === 'down' ) rico.y += 4;
-	}
- 
-	requestAnimationFrame( main );
-}
-//ページと依存（いぞん）している全てのデータが読み込まれたら、メインループ開始
-addEventListener('load', main(), false);
-
-//キーボードが押されたときに呼び出される関数（かんすう）
-function keydownfunc( event ) {
-	let key_code = event.keyCode;
-	if( key_code === 37 ) key.left = true;
-	if( key_code === 38 ) key.up = true;
-	if( key_code === 39 ) key.right = true;
-	if( key_code === 40 ) key.down = true;
-	event.preventDefault();
-}
-
-//キーボードが放（はな）されたときに呼び出される関数
-function keyupfunc( event ) {
-	let key_code = event.keyCode;
-	if( key_code === 37 ) key.left = false;
-	if( key_code === 38 ) key.up = false;
-	if( key_code === 39 ) key.right = false;
-	if( key_code === 40 ) key.down = false;
-}
+function displayCalendar() {
+    // 選択された月を取得する
+    const monthSelect = document.getElementById("monthSelect");
+    const monthIndex = monthSelect.selectedIndex;
+    const monthValue = monthSelect.options[monthIndex].value;
+    
+    // 指定された月の最初の日を取得する
+    const date = new Date();
+    date.setMonth(monthValue);
+    date.setDate(1);
+    
+    // カレンダーのHTMLを作成する
+    let calendarHTML = "<table>";
+    calendarHTML += "<tr><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr>";
+    
+    // カレンダーの各日付についてループ処理する
+    while (date.getMonth() == monthValue) {
+      calendarHTML += "<tr>";
+      for (let i = 0; i < 7; i++) {
+        if (date.getDay() == i) {
+          calendarHTML += "<td>" + date.getDate() + "</td>";
+          date.setDate(date.getDate() + 1);
+        } else {
+          calendarHTML += "<td></td>";
+        }
+      }
+      calendarHTML += "</tr>";
+    }
+    
+    calendarHTML += "</table>";
+    
+    // カレンダーを表示する
+    document.getElementById("calendar").innerHTML = calendarHTML;
+  }
+  

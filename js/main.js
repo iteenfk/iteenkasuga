dc69f3f48ad99e2991ddeab0e0e0da803cc75990
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-03-21 21:57:25 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
-function displayCalendar() {
-    // 選択された月を取得する
-    const monthSelect = document.getElementById("monthSelect");
-    const monthIndex = monthSelect.selectedIndex;
-    const monthValue = monthSelect.options[monthIndex].value;
-    
-    // 指定された月の最初の日を取得する
-    const date = new Date();
-    date.setMonth(monthValue);
-    date.setDate(1);
-    
-    // カレンダーのHTMLを作成する
-    let calendarHTML = "<table>";
-    calendarHTML += "<tr><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr>";
-    
-    // カレンダーの各日付についてループ処理する
-    while (date.getMonth() == monthValue) {
-      calendarHTML += "<tr>";
-      for (let i = 0; i < 7; i++) {
-        if (date.getDay() == i) {
-          calendarHTML += "<td>" + date.getDate() + "</td>";
-          date.setDate(date.getDate() + 1);
-        } else {
-          calendarHTML += "<td></td>";
-        }
-      }
-      calendarHTML += "</tr>";
-    }
-    
-    calendarHTML += "</table>";
-    
-    // カレンダーを表示する
-    document.getElementById("calendar").innerHTML = calendarHTML;
-  }
-  
+// canvas要素の取得
+const canvas = document.querySelector("#canvas");
+
+// 描画コンテキストの取得
+const context = canvas.getContext("2d");
+
+// 描画スタイルの初期化
+context.lineWidth = 5;
+context.lineJoin = "round";
+context.lineCap = "round";
+context.strokeStyle = "#000";
+
+// 描画状態の初期化
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+// タッチの動きを検知して描画を行う関数
+function draw(e) {
+  if (!isDrawing) return;
+  context.beginPath();
+  context.moveTo(lastX, lastY);
+  context.lineTo(e.touches[0].clientX, e.touches[0].clientY);
+  context.stroke();
+  [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
+}
+
+// タッチ開始時の処理
+canvas.addEventListener("touchstart", e => {
+  isDrawing = true;
+  [lastX, lastY] = [e.touches[0].clientX, e.touches[0].clientY];
+});
+
+// タッチ移動時の処理
+canvas.addEventListener("touchmove", draw);
+
+// タッチ終了時の処理
+canvas.addEventListener("touchend", () => (isDrawing = false));
+
+// タッチキャンセル時の処理
+canvas.addEventListener("touchcancel", () => (isDrawing = false));
